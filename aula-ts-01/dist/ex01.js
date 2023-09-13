8a5784055620dab1197cc233d0516c087681ba7f
Caio Neme <caiocesarneme@gmail.com>
2023-09-12 21:24:20 -0300

    Aula:5,6,7,8

---
+++ aula-ts-01/dist/ex01.js
@@ -0,0 +1,12 @@
+//! Problemas que o TS consegue resolver:
+//!   - 1: Tipagem Forte
+//!   - 2:
+//? Duvidas:
+//? apareceu esse erro
+//? SyntaxError: Unexpected token ':'
+
+function somar(num1, num2) {
+  return num1 + num2;
+}
+var resultado = somar(1, 3);
+console.log(resultado);
